c56da4ff0da19428921fcea77fc4e5b5ee71ae82
Sergio Di Martino <sergio@webdevserg.io>
2020-04-27 10:48:06 -0700

    Adds heroku port

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const routes = require('./routes');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
