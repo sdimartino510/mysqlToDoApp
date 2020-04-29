215023932242d3c1697edffc9ce64a17c513b2bc
Sergio Di Martino <sergio@webdevserg.io>
2020-04-29 14:46:55 -0700

    Cleans up code

--- client/src/components/RenderTodoList/index.js
+++ client/src/components/RenderTodoList/index.js
@@ -10,7 +10,9 @@ const RenderTodoList = props => {
       return props.items.map(todo => {
         return (
           <div key={ todo.id }>
-            <Link to={`/todos/${todo.id}`}><li style={{ color: todo.completed ? 'green' : 'red' }} >{todo.text}</li></Link>
+            <Link to={`/todos/${todo.id}`}>
+              <li style={{ color: todo.completed ? 'green' : 'red' }} >{ todo.text }</li>
+            </Link>
             <button onClick={ () => props.handleDelete(todo.id) }>Delete</button>
             <button onClick={ () => props.handleComplete(todo.id) }>Complete</button>
           </div>
